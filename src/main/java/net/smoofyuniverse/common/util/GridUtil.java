8805b2d0cd4886f61cf8bf4a12ff5ea286c24032
Yeregorix <yeregorixcraft@gmail.com>
2021-10-03 18:17:49 +0200

    Rename createRow -> row and createColumn -> column

--- src/main/java/net/smoofyuniverse/common/util/GridUtil.java
+++ src/main/java/net/smoofyuniverse/common/util/GridUtil.java
@@ -27,45 +27,45 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 
 public class GridUtil {
-	public static ColumnConstraints createColumn() {
+	public static ColumnConstraints column() {
 		return new ColumnConstraints();
 	}
 
-	public static ColumnConstraints createColumn(Priority p) {
+	public static ColumnConstraints column(Priority p) {
 		ColumnConstraints c = new ColumnConstraints();
 		c.setHgrow(p);
 		return c;
 	}
 
-	public static ColumnConstraints createColumn(boolean fill) {
+	public static ColumnConstraints column(boolean fill) {
 		ColumnConstraints c = new ColumnConstraints();
 		c.setFillWidth(fill);
 		return c;
 	}
 
-	public static ColumnConstraints createColumn(double percent) {
+	public static ColumnConstraints column(double percent) {
 		ColumnConstraints c = new ColumnConstraints();
 		c.setPercentWidth(percent);
 		return c;
 	}
 
-	public static RowConstraints createRow() {
+	public static RowConstraints row() {
 		return new RowConstraints();
 	}
 
-	public static RowConstraints createRow(Priority p) {
+	public static RowConstraints row(Priority p) {
 		RowConstraints r = new RowConstraints();
 		r.setVgrow(p);
 		return r;
 	}
 
-	public static RowConstraints createRow(boolean fill) {
+	public static RowConstraints row(boolean fill) {
 		RowConstraints r = new RowConstraints();
 		r.setFillHeight(fill);
 		return r;
 	}
 
-	public static RowConstraints createRow(double percent) {
+	public static RowConstraints row(double percent) {
 		RowConstraints r = new RowConstraints();
 		r.setPercentHeight(percent);
 		return r;
